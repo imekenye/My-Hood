75fa4e85797cec0744c20ab938560c032d510d8f
imekenye <ismekenye@gmail.com>
2019-06-03 14:58:43 -0700

    register business,post,and neighbourhood models

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from.models import Business,NeighbourHood,Post
 
 # Register your models here.
+admin.site.register(Business)
+admin.site.register(NeighbourHood)
+admin.site.register(Post)
