ee5cc253beaaee7e6024066b437b92e21634575e
imekenye <ismekenye@gmail.com>
2019-06-03 10:46:08 -0700

    create profile model class

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from PIL import Image
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_email = models.EmailField(max_length=70, blank=True)
+    myhood_id = models.ForeignKey('pages.NeighbourHood', blank=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
