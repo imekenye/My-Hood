8023af6b1f38ab77eb689d1060d70cbd60555ccd
imekenye <ismekenye@gmail.com>
2019-06-02 17:28:58 -0700

    add accounts app

--- myhood/settings.py
+++ myhood/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
