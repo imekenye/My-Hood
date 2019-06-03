274bbd9866576fd7e8838407727cc2b08d16018f
imekenye <ismekenye@gmail.com>
2019-06-03 10:51:12 -0700

    add view functions to pages app

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,61 @@
 from django.shortcuts import render
+from accounts.models import Profile
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
-def index(request):
-    return render(request, 'pages/index.html')
+# def index(request):
+#     return render(request, 'pages/index.html')
+
+def home(request):
+    context = {
+
+        'posts': Post.objects.all()
+    }
+    return render(request, 'pages/index.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'pages/index.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'post_image', 'content','url']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'post_image', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
