50afd20b3cdf72856b0e6ac4822433b5b006aa3a
imekenye <ismekenye@gmail.com>
2019-06-03 14:59:13 -0700

    update forms file

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,4 +23,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['myhood_id']
+        fields = ['hood']
