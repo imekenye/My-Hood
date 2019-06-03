ea0bb706cd9808d861abf051323fa937b5e0b3b5
imekenye <ismekenye@gmail.com>
2019-06-03 14:59:45 -0700

    update models

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 from PIL import Image
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_email = models.EmailField(max_length=70, blank=True)
-    myhood_id = models.ForeignKey('pages.NeighbourHood', blank=True,on_delete=models.CASCADE)
+    hood = models.ForeignKey('pages.NeighbourHood', on_delete=models.CASCADE, related_name="profile_hood", null=True)
+
 
     def __str__(self):
         return f'{self.user.username} Profile'
