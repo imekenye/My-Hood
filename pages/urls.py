c8cb1b7f842cc924422e377c59da1a870682adcc
imekenye <ismekenye@gmail.com>
2019-06-03 10:49:51 -0700

    add url pattern

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from .views import PostDetailView, PostCreateView, PostUpdateView
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('', views.home, name='home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostUpdateView.as_view(), name='post-delete'),
+    path('post/new', PostCreateView.as_view(), name='post-create'),
+
 ]
